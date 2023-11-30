ecbc49631575796ec3e4c8b286a7c9df541c2bcd
wangzhangsheng <wb-wzs728893@alibaba-inc.com>
2023-11-30 10:41:55 +0800

    fix: update

--- lua/SingleComment/init.lua
+++ lua/SingleComment/init.lua
@@ -1,146 +1,57 @@
-local M = {}
 local Util = require "SingleComment.util"
+local M = {}
 
----@type table kinds of comments
--- stylua: ignore
-local comments = {
-  ---@type table lines and filetypes that can be changed to block comments
-  -- some can't be changed ;-; but are here for format adjustments
-  block = {
-    ["<!--"]     = { "<!-- ", " -->" },
-    ["--[["]     = { "--[[ ", " ]]" },
-    ["-- "]      = { "--[[ ", " ]]" },
-    ["--"]       = { "--[[ ", " ]]" },
-    ["/*"]       = { "/* ", " */" },
-    lisp         = { "#| ", " |#" },
-    cmake        = { "#[[ ", " ]]" },
-    haskell      = { "{- ", " -}" },
-    elm          = { "{- ", " -}" },
-    julia        = { "#= ", " =#" },
-    luau         = { "--[[ ", " ]]" },
-    nim          = { "#[ ", " ]#" },
-    ocaml        = { "(* ", " *)" },
-    fsharp       = { "(* ", " *)" },
-    markdown     = { "<!-- ", " -->" },
-    org          = { "# ", "" },
-    neorg        = { "# ", "" },
-    javascript   = { "/* ", " */" },
-    editorconfig = { "# ", "" },
-    fortran      = { "! ", "" },
-    default      = { "/* ", " */" },
-  },
-  ---@type table blocks and filetypes that can be changed to line comments
-  -- some can't be changed ;-; but are here for format adjustments
-  line = {
-    ["<!--"]     = { "<!-- ", " -->" },
-    ["/*"]       = { "// ", "" },
-    ["/* "]      = { "// ", "" },
-    [";"]        = { "; ", "" },
-    ["%"]        = { "% ", "" },
-    ['#']        = { "# ", "" },
-    nim          = { "# ", "" },
-    json         = { "// ", "" },
-    jsonc        = { "// ", "" },
-    nelua        = { "-- ", "" },
-    luau         = { "-- ", "" },
-    ocaml        = { "(* ", " *)" },
-    css          = { "/* ", " */" },
-    markdown     = { "<!-- ", " -->" },
-    org          = { "# ", "" },
-    neorg        = { "# ", "" },
-    editorconfig = { "# ", "" },
-    fortran      = { "! ", "" },
-    default      = { "// ", "" },
-  },
-}
+M.setup = function()
+  _G.__MiniComment = M
 
----@param kind? string kind of returned comment, defaults to "line"
----@return table table with the comment beginning/end
-function M.GetComment(kind)
-  kind = kind or "line"
-  local comment = {}
-  local bufnr = vim.api.nvim_get_current_buf()
-  local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-
-  local ok, tsc = pcall(require, "ts_context_commentstring.internal")
-  local cs = ""
-  if ok then
-    tsc.update_commentstring({})
-  end
-
-  if comments[kind][filetype] ~= nil then
-    -- use [filetype] override
-    comment = comments[kind][filetype]
-  elseif cs == "" or cs == nil then
-    -- use [default] comment for [kind]
-    comment = comments[kind]["default"]
-  else
-    vim.g.a = cs
-    -- separating strings like `%%s` like tex comments
-    -- does not work well in a for loop with gmatch
-    comment[1] = cs:match("(.*)%%s")
-    comment[2] = cs:match("%%s(.*)")
-
-    -- use a better [kind] of comment, or adjust its format
-    if comments[kind][comment[1]] then
-      comment = comments[kind][comment[1]]
-    end
-
-    if comment[1] == nil then
-      comment[1] = ""
-    end
-
-    if comment[2] == nil then
-      comment[2] = ""
-    end
-  end
-
-  return comment
+  vim.keymap.set("n", "gcc", function() return M.operator() .. '_' end, { expr = true })
+  -- Using `:<c-u>` instead of `<cmd>` as latter results into executing before
+  -- proper update of `'<` and `'>` marks which is needed to work correctly.
+  vim.keymap.set("x", "gc", [[:<c-u>lua __MiniComment.operator('visual')<cr>]], {})
 end
 
--- TODO: 我看 comment str 拿不准的原因可能是 visual mode
--- 之后鼠标的位置导致调用的 ts_context_commentstring 的函数的
--- 时机不一致
-function M.BlockComment()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local comment = M.GetComment("block")
-  local _, sr, sc, _ = unpack(vim.fn.getpos("."))
-  local _, er, ec, _ = unpack(vim.fn.getpos("v"))
-  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  local bufnr = vim.api.nvim_get_current_buf()
-  local cs = vim.api.nvim_buf_get_option(bufnr, "commentstring")
-  vim.g.cs = cs
-
-  -- keep start/end in the right place in reverse selection
-  if sr > er then
-    sr, er = er, sr
-    sc, ec = ec, sc
+M.operator = function(mode)
+  -- single line comment
+  if mode == nil then
+    vim.o.operatorfunc = 'v:lua.__MiniComment.operator'
+    return 'g@'
   end
 
-  lines = M.CommentPairs(lines, sr, er, comment)
-
-  if sr == er then
-  else
+  local mark_left, mark_right = '[', ']'
+  if mode == 'visual' then
+    mark_left, mark_right = '<', '>'
   end
 
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-  --  vim.api.nvim_feedkeys("=", "n", false)
+  local line_left, col_left = unpack(vim.api.nvim_buf_get_mark(0, mark_left))
+  local line_right, col_right = unpack(vim.api.nvim_buf_get_mark(0, mark_right))
+
+  if (line_left > line_right) or (line_left == line_right and col_left > col_right) then return end
+  -- Use `vim.cmd()` wrapper to allow usage of `lockmarks` command, because raw
+  -- execution deletes marks in region (due to `vim.api.nvim_buf_set_lines()`).
+  vim.cmd(string.format(
+    [[lockmarks lua __MiniComment.toggle_lines(%d, %d)]],
+    line_left,
+    line_right
+  ))
+  return ''
 end
 
-function M.CommentPairs(lines, sr, er, comment)
-  -- if comment string comes is paired
-  -- then comment the start of the block and
-  -- the end of the block
-  -- otherwise comment each line of the block
-  if (lines[sr]:find(vim.pesc(comment[1]))) then
-    -- uncomment it
-    --Util.remove_comment_multiline(lines, sr, er, comment[1], comment[2])
-  else
-    -- comment it
-    --Util.insert_comment_multiline(lines, sr, er, comment[1], comment[2])
-  end
+M.toggle_lines = function(line_start, line_end)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local comment_parts = M.make_comment_parts()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, line_start - 1, line_end, false)
+  Util.toggle_comment(lines, 1, #lines, { comment_parts.left, comment_parts.right })
+  vim.api.nvim_buf_set_lines(bufnr, line_start - 1, line_end, false, lines)
+end
 
-  return lines
+M.make_comment_parts = function()
+  local cs = require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
+
+  -- Assumed structure of 'commentstring':
+  -- <space> <left> <'%s'> <right> <space>
+  -- So this extracts parts without surrounding whitespace
+  local left, right = cs:match('^%s*(.*)%%s(.-)%s*$')
+  return { left = left, right = right }
 end
 
 return M
